e70a37e06ed68db83aa0a8f748f326ca0b28eaed
AgungBahtiarr <agungbahtiar050@gmail.com>
2025-01-09 13:26:22 +0700

    refactor user model and migration to use User::createMany for seeding

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ class User extends Model implements AuthenticatableContract
     use Authenticatable;
     use HasFactory;
     protected $table = "users";  // Nama tabel tetap 'users'
-    protected $primaryKey = "id_user";  // Primary key menggunakan 'id_user'
+    // protected $primaryKey = "id_user";  // Primary key menggunakan 'id_user'
     public $incrementing = true;  // Jika id_user adalah auto-increment
     protected $keyType = 'int';  // Jika id_user adalah integer
 
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,15 +13,23 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
+        User::createMany([
+            [
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => 'User',
+                'email' => 'user@user.com',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => 'Staff',
+                'email' => 'staff@staff.com',
+                'password' => Hash::make('password'),
+            ]
+        ]);
     }
 
     /**
